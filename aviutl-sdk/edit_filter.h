fe6a1c5b2cd1d6158004ae1b394564f787521d50
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2020-11-19 20:06:15 +0900

    プロジェクト初期化 1. プロジェクト作成 2. AviUtl SDKの配置 3. Discord Game SDKの配置 4. メインファイル作成

---
+++ aviutl-sdk/edit_filter.h
@@ -0,0 +1,9 @@
+#define	MID_EDIT_VIDEO_COPY		100
+#define	MID_EDIT_AUDIO_COPY		101
+#define	MID_EDIT_COPY			102
+#define	MID_EDIT_PASTE			103
+#define	MID_EDIT_DELETE			104
+#define	MID_EDIT_INSERT			105
+#define	MID_EDIT_FILE_INFO		106
+
+
